50ace4efcaeaf6c73970e0458db5f3da406cc566
Arjwan <arjwanalqarni77@gmail.com>
2025-09-15 09:51:54 +0300

    First commit - BethanysPieShopAdmin

---
+++ Migrations/20250908084423_AddDescriptionArabicToPie.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BethanysPieShopAdmin.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDescriptionArabicToPie : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DescriptionArabic",
+                table: "Pies",
+                type: "nvarchar(1000)",
+                maxLength: 1000,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DescriptionArabic",
+                table: "Pies");
+        }
+    }
+}
